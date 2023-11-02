093dadbe73190aaf4acda427655398c23da0542f
Parihsz <jlu1@bayviewglen.ca>
2023-11-01 20:50:40 -0400

    2022 j3

---
+++ 2022/Junior/J3.py
@@ -0,0 +1,18 @@
+tune = input()
+output = ""
+tuned_index = 999999999
+for x in range(len(tune)):
+    i = tune[x]
+    if i == "+":
+        output += " tighten "
+        tuned_index = x
+    elif i == "-":
+        output += " loosen "
+        tuned_index = x
+    else:
+        if tuned_index + 1 == x:
+            output += i
+            output += "\n" 
+        else:
+            output += i
+print(output)
